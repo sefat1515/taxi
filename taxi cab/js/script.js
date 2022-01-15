bf18f8f35ca1f36763530c7467b3c228cf317d69
sefat1515 <91536653+sefat1515@users.noreply.github.com>
2022-01-15 18:39:09 +0600

    Add files via upload

---
+++ taxi cab/js/script.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+    
+    $( "#accordion" ).accordion({
+      collapsible: true
+    });
+    // wow
+     wow = new WOW(
+        {
+        boxClass:     'wow',      // default
+        animateClass: 'animated', // default
+        offset:       0,          // default
+        mobile:       true,       // default
+        live:         true        // default
+      }
+      )
+      wow.init();
+
+      // aos
+    AOS.init({
+        duration: 1200,
+    })
+
+});
